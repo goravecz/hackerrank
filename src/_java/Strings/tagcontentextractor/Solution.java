36b496e913380fbdb6b4c8fc7ee9613997d8084b
goravecz <oravecz.gabor@protonmail.com>
2018-06-20 16:05:49 +0200

    Add last Java Strings solution

---
+++ src/_java/Strings/tagcontentextractor/Solution.java
@@ -0,0 +1,34 @@
+package _java.Strings.tagcontentextractor;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * https://www.hackerrank.com/challenges/tag-content-extractor/problem
+ */
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String line = in.nextLine();
+            String regex = "<(.+)>([^<]+)</\\1>";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(line);
+            boolean match = false;
+
+            while (matcher.find()) {
+                System.out.println(matcher.group(2));
+                match = true;
+            }
+            if (!match) {
+                System.out.println("None");
+            }
+            testCases--;
+        }
+        in.close();
+    }
+}
