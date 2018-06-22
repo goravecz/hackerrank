0c53025c355af3ab4df0eecc9ba2917095f03438
goravecz <oravecz.gabor@protonmail.com>
2018-06-22 22:52:49 +0200

    Add Java Data Structures solution

---
+++ src/_java/datastructures/javasubarrays/Solution.java
@@ -0,0 +1,47 @@
+package _java.datastructures.javasubarrays;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/java-negative-subarray/problem
+ */
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] array = readInput();
+        System.out.println(countNegativeSubArrays(array));
+    }
+
+    private static int[] readInput() {
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        return array;
+    }
+
+    private static int countNegativeSubArrays(int[] array) {
+        int count = 0;
+        int subArrayLength = 1;
+        int arrayLength = array.length;
+
+        while (subArrayLength <= arrayLength) {
+            for (int i = 0; i <= arrayLength - subArrayLength; i++) {
+                int sum = 0;
+                for (int j = 0; j < subArrayLength; j++) {
+                    sum = sum + array[i + j];
+                }
+                if (sum < 0) {
+                    count++;
+                }
+            }
+            subArrayLength++;
+        }
+
+        return count;
+    }
+}
