52d0543157431439016aa8fb670a2deaff650cc2
goravecz <oravecz.gabor@protonmail.com>
2018-06-29 17:00:03 +0200

    Add Java Data Structures solutions

---
+++ src/_java/datastructures/javacomparator/Solution.java
@@ -0,0 +1,29 @@
+package _java.datastructures.javacomparator;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/java-comparator/problem
+ */
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for (int i = 0; i < n; i++) {
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for (int i = 0; i < player.length; i++) {
+            System.out.printf("%s %s\n", player[i].getName(), player[i].getScore());
+        }
+    }
+}
---
+++ src/_java/datastructures/javagenerics/Printer.java
@@ -0,0 +1,10 @@
+package _java.datastructures.javagenerics;
+
+public class Printer {
+
+    public static <T> void printArray(T[] array ) {
+        for (T element : array) {
+            System.out.println(element);
+        }
+    }
+}
